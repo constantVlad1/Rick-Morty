ec086a5c6bf6818a88cd577c315d8cbc59cad1f6
constantVlad1 <127964911+constantVlad1@users.noreply.github.com>
2023-07-06 21:14:21 +0000

    F

--- scripts/script.js
+++ scripts/script.js
@@ -10,23 +10,30 @@ pHeader.innerText="Rick&Morty";
 
 const divRoot = document.querySelector("#root");
 
-const firstDiv = document.createElement("div");
-firstDiv.classList.add("sidebar");
-divRoot.appendChild(firstDiv);
+const sideBar = document.createElement("div");
+sideBar.classList.add("sidebar");
+divRoot.appendChild(sideBar);
 
-const h1SideBar = document.createElement("h1");
-h1SideBar.innerText = "Episodes:";
-firstDiv.appendChild(h1SideBar)
+const episodsDiv = document.createElement("div");
+episodsDiv.classList.add("episodsDiv");
+sideBar.prepend(episodsDiv);
 
 fetch("https://rickandmortyapi.com/api/episode")
 .then( respons => respons.json())
 .then(data => console.log(data.results.forEach((episod) => {
     const aLink = document.createElement("a");
+    aLink.href = "#"
     aLink.classList.add("episodNames");
-    aLink.innerText = "Episod" + " " + (episod.id);
-    firstDiv.appendChild(aLink)
+    aLink.innerText = "Episode" + " " + (episod.id);
+    episodsDiv.appendChild(aLink)
 })))
+
+const buttonPage = document.createElement("button");
+buttonPage.id = "buttonPage";
+sideBar.appendChild(buttonPage);
+buttonPage.innerText = "Load Episodes "
+
 
 const mainBlock = document.createElement("div");
 divRoot.appendChild(mainBlock);
-mainBlock.classList.add("mainBlock")
+mainBlock.classList.add("mainBlock");
