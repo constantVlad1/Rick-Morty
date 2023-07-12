b334dd859b1f17bd21fd8fed9181a3940c58c2dd
constantVlad1 <127964911+constantVlad1@users.noreply.github.com>
2023-07-12 21:02:21 +0000

    F

--- scripts/script.js
+++ scripts/script.js
@@ -18,20 +18,46 @@ const episodsDiv = document.createElement("div");
 episodsDiv.classList.add("episodsDiv");
 sideBar.prepend(episodsDiv);
 
-fetch("https://rickandmortyapi.com/api/episode")
-.then( respons => respons.json())
-.then(data => console.log(data.results.forEach((episod) => {
+function sideBarFetch(url){
+ episodsDiv.innerHTML = "";
+ fetch(url)
+ .then( respons => respons.json())
+ .then(data => console.log(data.results.forEach((episod) => {
     const aLink = document.createElement("a");
     aLink.href = "#"
     aLink.classList.add("episodNames");
-    aLink.innerText = "Episode" + " " + (episod.id);
+    aLink.innerText = episod.name
     episodsDiv.appendChild(aLink)
 })))
+}
+
+let currentPage = 1;
+function addMoreEpisodios () {
+    fetch(`https://rickandmortyapi.com/api/episode?page=${currentPage}`)
+    .then( respons => respons.json())
+    .then(data => data.results.forEach((episod) => {
+    const aLink = document.createElement("a");
+    aLink.href = "#"
+    aLink.classList.add("episodNames");
+    aLink.innerText = episod.name
+    episodsDiv.appendChild(aLink)
+}));
+currentPage++;
+if(currentPage > 4) {
+    alert("Stop")
+};
+}
+addMoreEpisodios();
 
 const buttonPage = document.createElement("button");
 buttonPage.id = "buttonPage";
 sideBar.appendChild(buttonPage);
-buttonPage.innerText = "Load Episodes "
+buttonPage.innerText = "Load Episodes ";
+buttonPage.addEventListener("click", (event) => {
+    event.preventDefault();
+    addMoreEpisodios();
+})
+
 
 
 const mainBlock = document.createElement("div");
